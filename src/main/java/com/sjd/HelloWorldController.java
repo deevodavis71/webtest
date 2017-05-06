6c32537fc8e9d1bf9d874024e7f16bc7767f8bb4
Steve Davis <davissa@UKWY300671.local>
2017-05-06 17:31:02 +0100

    Initial Commit

---
+++ src/main/java/com/sjd/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.sjd;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping (path = "/api")
+public class HelloWorldController
+{
+    @RequestMapping (path = "/sayHello/{name}")
+    public String sayHello (@PathVariable (name = "name") String name)
+    {
+        return "Hello, " + name + ".";
+    }
+
+    @RequestMapping (path = "/sayGoodbye/{name}")
+    public String sayGoodbye (@PathVariable (name = "name") String name)
+    {
+        return "Goodbye " + name + ".";
+    }
+}
